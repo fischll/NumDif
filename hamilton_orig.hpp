79199acfe973e6280d0c9416db3edc1feeabb846
Lorenz Fischl <e1625248@student.tuwien.ac.at>
2018-04-28 22:16:38 +0200

    XKKXKXKKXKKKXKXKXKKXKKXKXKKXKXKXKXKXXXKX

--- hamilton_orig.hpp
+++ hamilton_orig.hpp
@@ -10,65 +10,65 @@ public:
     return EvalV(q) + EvalT(p);
   }
   virtual void EvalDV(const Vector<> & q, Vector<> & DV) const{
-		// numerical differentiation
-		int n = q.Size();
-		Vector<> qr(n), ql(n);
-		double Vr(n), Vl(n);
-		double eps = 1e-6;
-		for (int i = 0; i < n; i++)
-		{
-			ql = q;  ql(i) -= eps;
-			qr = q;  qr(i) += eps;
-			Vl = EvalV(ql);
-			Vr = EvalV(qr);
-			DV(i) = 1.0 / (2 * eps) * (Vr - Vl);
-		}
-	}
+    // numerical differentiation
+    int n = q.Size();
+    Vector<> qr(n), ql(n);
+    double Vr(n), Vl(n);
+    double eps = 1e-6;
+    for (int i = 0; i < n; i++)
+      {
+	ql = q;  ql(i) -= eps;
+	qr = q;  qr(i) += eps;
+	Vl = EvalV(ql);
+	Vr = EvalV(qr);
+	DV(i) = 1.0 / (2 * eps) * (Vr - Vl);
+      }
+  }
   virtual void EvalDT(const Vector<> & p, Vector<> & DT) const{
-		// numerical differentiation
-		int n = p.Size();
-		Vector<> pr(n), pl(n);
-		double Tr(n), Tl(n);
-		double eps = 1e-6;
-		for (int i = 0; i < n; i++)
-		{
-			pl = p;  pl(i) -= eps;
-			pr = p;  pr(i) += eps;
-			Tl = EvalT(pl);
-			Tr = EvalT(pr);
-			DT(i) = 1.0 / (2 * eps) * (Tr - Tl);
-		}
-	}
+    // numerical differentiation
+    int n = p.Size();
+    Vector<> pr(n), pl(n);
+    double Tr(n), Tl(n);
+    double eps = 1e-6;
+    for (int i = 0; i < n; i++)
+      {
+	pl = p;  pl(i) -= eps;
+	pr = p;  pr(i) += eps;
+	Tl = EvalT(pl);
+	Tr = EvalT(pr);
+	DT(i) = 1.0 / (2 * eps) * (Tr - Tl);
+      }
+  }
   /*virtual void EvaldHdp(const Vector<> & p, const Vector<> & q, Vector<> & dHdp)	{
-		// numerical differentiation
-		int n = q.Size();
-		Vector<> pr(n), pl(n);
-		double Hr(n), Hl(n);
-		double eps = 1e-6;
-		for (int i = 0; i < n; i++)
-		{
-			pl = p;  pl(i) -= eps;
-			pr = p;  pr(i) += eps;
-			Hl = Eval(pl, q);
-			Hr = Eval(pr, q);
-			dHdp(i) = 1.0 / (2 * eps) * (Hr - Hl);
-		}
-	}
+  // numerical differentiation
+  int n = q.Size();
+  Vector<> pr(n), pl(n);
+  double Hr(n), Hl(n);
+  double eps = 1e-6;
+  for (int i = 0; i < n; i++)
+  {
+  pl = p;  pl(i) -= eps;
+  pr = p;  pr(i) += eps;
+  Hl = Eval(pl, q);
+  Hr = Eval(pr, q);
+  dHdp(i) = 1.0 / (2 * eps) * (Hr - Hl);
+  }
+  }
   virtual void EvaldHdq(const Vector<> & p, const Vector<> & q, Vector<> & dHdq)	{
-		// numerical differentiation
-		int n = p.Size();
-		Vector<> qr(n), ql(n);
-		double Hr(n), Hl(n);
-		double eps = 1e-6;
-		for (int i = 0; i < n; i++)
-		{
-			ql = q;  ql(i) -= eps;
-			qr = q;  qr(i) += eps;
-			Hl = Eval(p, ql);
-			Hr = Eval(p, qr);
-			dHdq(i) = 1.0 / (2 * eps) * (Hr - Hl);
-		}
-	}*/
+  // numerical differentiation
+  int n = p.Size();
+  Vector<> qr(n), ql(n);
+  double Hr(n), Hl(n);
+  double eps = 1e-6;
+  for (int i = 0; i < n; i++)
+  {
+  ql = q;  ql(i) -= eps;
+  qr = q;  qr(i) += eps;
+  Hl = Eval(p, ql);
+  Hr = Eval(p, qr);
+  dHdq(i) = 1.0 / (2 * eps) * (Hr - Hl);
+  }
+  }*/
 };
 
 
@@ -76,11 +76,11 @@ public:
 class SSM_Hamilton
 {
 public:
-	// do the step
-	virtual bool Step(double h, const Hamilton_Function & func,
-			  const Vector<> & pold, const Vector<> & qold, Vector<> & pnew, Vector<> & qnew) = 0;
-	//virtual void StepAdaptive(double t, double hold, double hnew, const ODE_Function & func, const Vector<> & yold, const Vector<> & ynew) = 0;
-	virtual int Order() = 0;
+  // do the step
+  virtual bool Step(double h, const Hamilton_Function & func,
+		    const Vector<> & pold, const Vector<> & qold, Vector<> & pnew, Vector<> & qnew) = 0;
+  //virtual void StepAdaptive(double t, double hold, double hnew, const ODE_Function & func, const Vector<> & yold, const Vector<> & ynew) = 0;
+  virtual int Order() = 0;
 };
 
 // the time integration loop
@@ -88,35 +88,35 @@ void ODESolver_Hamilton(const Hamilton_Function & func, SSM_Hamilton & ssm,
 			double t0, Vector<> & p0, Vector<> & q0, double tend, double h,
 			ostream & out, size_t writeout_stepsize = 1)
 {
-	double t = t0;
-	int n = p0.Size();
-	Vector<> pold(n), pnew(n), qold(n), qnew(n);
-	pold=p0;
-	qold=q0;
-	size_t step = 0;
-	while (t < tend)
+  double t = t0;
+  int n = p0.Size();
+  Vector<> pold(n), pnew(n), qold(n), qnew(n);
+  pold=p0;
+  qold=q0;
+  size_t step = 0;
+  while (t < tend)
+    {
+      if (step%writeout_stepsize == 0)
 	{
-		if (step%writeout_stepsize == 0)
-		{
-			out << t;
-			for (int i = 0; i < n; i++)
-			  out << " " << pold(i) << " " << qold(i);
-			out << "\n";
-		}
-		ssm.Step(h, func, pold, qold, pnew, qnew);
-		pold=pnew;
-		qold=qnew;
-		t += h; step++;
+	  out << t;
+	  for (int i = 0; i < n; i++)
+	    out << " " << pold(i) << " " << qold(i);
+	  out << "\n";
 	}
+      ssm.Step(h, func, pold, qold, pnew, qnew);
+      pold=pnew;
+      qold=qnew;
+      t += h; step++;
+    }
 }
 
 
-class Symplectic_Euler : public SSM_Hamilton
+class SymplecticEV : public SSM_Hamilton
 {
   Vector<> DV, DT;
 public:
   virtual bool Step( double h, const Hamilton_Function & func,
-			  const Vector<> & pold, const Vector<> & qold, Vector<> & pnew, Vector<> & qnew) override
+		     const Vector<> & pold, const Vector<> & qold, Vector<> & pnew, Vector<> & qnew) override
   {
     DV.SetSize(pold.Size());
     DT.SetSize(qold.Size());
--- pendulum_Hamilton.cpp
+++ pendulum_Hamilton.cpp
@@ -6,33 +6,36 @@ using namespace ngbla;
 #include <math.h>
 
 
-class Pendulum_ODE_Function : public ODE_Function
+class Pendulum_Hamilton : public Hamilton_Function
 {
-	double g; //gravitation force
-	double l; //pendulum länge
 public:
-	Pendulum_ODE_Function(double am, double ak) { g = am; l = ak; }
+  Pendulum_Hamilton() { }
 
-	//alpha, omega
-	virtual void Eval(double t, const Vector<> & y, Vector<> & f) const
-	{
-		f(0) = y(1);
-		f(1) = -(g/l) * sin(y(0));
-	}
+  virtual double EvalV(const Vector<> & q) const
+  {
+    return -cos(q(1));
+  }
+  virtual double EvalT(const Vector<> & p) const
+  {
+    return 1./2*pow(p(1),2);
+  }
 };
 
 
 
 int main()
 {
-	ImplicitEuler impl_euler;
-	double h = 0.01;
+  SymplecticEV symplEV;
+  double h = 0.01;
+  double t0=0;
+  double tend = 10;
 
-	ofstream out("Pendulum.txt");
-	Pendulum_ODE_Function pen(1, 1);
-	Vector<> y0(2);
-	y0(0) = M_PI / 2; //das ist ja nur ein startwert und pi ging nicht
-	y0(1) = 0;
-	ODESolver(pen, impl_euler, 0, y0, 1000, h, out, 10);
-	return 0;
+  ofstream out("PendulumH.txt");
+  Pendulum_Hamilton pen();
+  Vector<> p0(1);
+  Vector<> q0(1);
+  p0=1.;
+  q0=0;
+  ODESolver_Hamilton(pen, symplEV, t0, p0, q0, tend, h, out, 1);
+  return 0;
 }
